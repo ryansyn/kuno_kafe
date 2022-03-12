caaa7a3c30db47c97dec80b03f0b6246c5d4de44
muta69 <90420703+muta69@users.noreply.github.com>
2022-03-11 19:26:29 -0600

    Create CHILD-rank_names.lua

---
+++ CHILD-rank_names.lua
@@ -0,0 +1,78 @@
+local module = {
+	[0] = {
+		["Name"] = "Guest",
+		["Tag"] = nil
+	},
+	
+	[1] = {
+		["Name"] = "Customer",
+		["Tag"] = nil
+	},
+	
+	[2] = {
+		["Name"] = "Guest",
+		["Tag"] = nil
+	},
+	
+	[3] = {
+		["Name"] = "Trainee",
+		["Tag"] = "LR"
+	},
+	
+	[4] = {
+		["Name"] = "Junior Barista",
+		["Tag"] = "LR"
+	},
+	
+	[5] = {
+		["Name"] = "Barista",
+		["Tag"] = "LR"
+	},
+	
+	[6] = {
+		["Name"] = "Senior Barista",
+		["Tag"] = "MR"
+	},
+	
+	[7] = {
+		["Name"] = "Staffing Intern",
+		["Tag"] = "HR"
+	},
+	
+	[8] = {
+		["Name"] = "Staffing Supervisor",
+		["Tag"] = "HR"
+	},
+	
+	[9] = {
+		["Name"] = "Staffing Manager",
+		["Tag"] = "HR"
+	},
+	
+	[10] = {
+		["Name"] = "Staffing Director",
+		["Tag"] = "SHR"
+	},
+	
+	[11] = {
+		["Name"] = "Executive",
+		["Tag"] = "SHR"
+	},
+	
+	[253] = {
+		["Name"] = "CCO",
+		["Tag"] = "X"
+	},
+	
+	[254] = {
+		["Name"] = "CEO",
+		["Tag"] = "X"
+	},
+	
+	[255] = {
+		["Name"] = "Group Owner",
+		["Tag"] = "X"
+	}
+}
+
+return module;
